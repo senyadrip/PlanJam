01c7e67d614c657be87e4b8e58b09f9f5499e80b
senyadrip <arsenymatveev@gmail.com>
2025-05-08 00:48:33 -0400

    setup mongodb + server

---
+++ backend/src/app.ts
@@ -0,0 +1,30 @@
+import express, { Application } from "express";
+import cors from "cors";
+import helmet from "helmet";
+
+const app: Application = express();
+
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use(express.json());
+
+app.use(
+    (
+      err: Error,
+      req: express.Request,
+      res: express.Response,
+      next: express.NextFunction
+    ) => {
+      console.error(err.stack);
+      res.status(500).send("Something broke!");
+    }
+);
+
+export default app;
---
+++ backend/src/server.ts
@@ -0,0 +1,12 @@
+import connectDB from "./config/db";
+import dotenv from 'dotenv';
+import app from "./app";
+
+dotenv.config();
+
+connectDB();
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
